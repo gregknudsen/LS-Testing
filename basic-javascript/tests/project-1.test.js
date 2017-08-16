b84f5baa98c23a224abd9e7a75f54942d86b861d
satishvattikuti <satishvattikuti94@gmail.com>
2017-08-15 21:20:50 -0400

    working through

--- basic-javascript/tests/project-1.test.js
+++ basic-javascript/tests/project-1.test.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const funcs = require('../src/project-1');
 
 // we've gone ahead and gotten a start here for you,
@@ -12,7 +13,22 @@ describe('Project-1 Functions', () => {
   describe('`multiplyByTen`', () => {
     it('should be a function', () => {
       const multiplyByTen = funcs.multiplyByTen;
-      assert.typeOf(multiplyByTen, 'object');
+      assert.typeOf(multiplyByTen, 'function');
+    });
+    it('Should return 20 when passed 2', () => {
+      const multiplyByTen = funcs.multiplyByTen;
+      const result = multiplyByTen(2);
+      expect(result).to.equal(20);
+    });
+    it('Should not equal to 10 when passed 2', () => {
+      const multiplyByTen = funcs.multiplyByTen;
+      const result = multiplyByTen(2);
+      expect(result).to.not.equal(10);
+    });
+    it('should return a number', () => {
+      const multiplyByTen = funcs.multiplyByTen;
+      const result = multiplyByTen(3);
+      assert.typeOf(result, 'number');
     });
     // begin here
   });
@@ -20,77 +36,178 @@ describe('Project-1 Functions', () => {
   describe('`subtractFive`', () => {
     it('should be a function', () => {
       const subtractFive = funcs.subtractFive;
-      assert.typeOf(subtractFive, 'array');
+      assert.typeOf(subtractFive, 'function');
+    });
+    it('should return 5 when passed 10', () => {
+      const subtractFive = funcs.subtractFive;
+      const result = subtractFive(10);
+      expect(result).to.equal(5);
+    });
+    it('should return a number', () => {
+      const subtractFive = funcs.subtractFive;
+      const result = subtractFive(10);
+      assert.typeOf(result, 'number');
     });
   });
 
   describe('`areSameLength`', () => {
     it('should be a function', () => {
       const areSameLength = funcs.areSameLength;
-      assert.typeOf(areSameLength, 'number');
+      assert.typeOf(areSameLength, 'function');
+    });
+    it('should return true when passed bat and cat', () => {
+      const areSameLength = funcs.areSameLength;
+      const res = areSameLength('bat', 'cat');
+      expect(res).to.be.true;
+    });
+    it('should return false when passed bat and caaat', () => {
+      const areSameLength = funcs.areSameLength;
+      const res = areSameLength('bat', 'caaat');
+      expect(res).to.be.false;
     });
   });
 
   describe('`areEqual`', () => {
     it('should be a function', () => {
       const areEqual = funcs.areEqual;
-      assert.typeOf(areEqual, 'number');
+      assert.typeOf(areEqual, 'function');
+    });
+    it('should be equal when passed same value', () => {
+      const areEqual = funcs.areEqual;
+      const res = areEqual(5, 5);
+      expect(res).to.be.true;
+    });
+    it('should return boolean', () => {
+      const areEqual = funcs.areEqual;
+      const res = areEqual(5, 5);
+      assert.typeOf(res, 'boolean');
     });
   });
 
   describe('`lessThanNinety`', () => {
     it('should be a function', () => {
       const lessThanNinety = funcs.lessThanNinety;
-      assert.typeOf(lessThanNinety, 'number');
+      assert.typeOf(lessThanNinety, 'function');
+    });
+    it('should return true if less than ninety', () => {
+      const lessThanNinety = funcs.lessThanNinety;
+      const res = lessThanNinety(80);
+      expect(res).to.be.true;
+    });
+    it('should return a boolean', () => {
+      const lessThanNinety = funcs.lessThanNinety;
+      const res = lessThanNinety(98);
+      assert.typeOf(res, 'boolean');
     });
   });
 
   describe('`greaterThanFifty`', () => {
     it('should be a function', () => {
       const greaterThanFifty = funcs.greaterThanFifty;
-      assert.typeOf(greaterThanFifty, 'number');
+      assert.typeOf(greaterThanFifty, 'function');
+    });
+    it('should be return true if greater than fifty', () => {
+      const greaterThanFifty = funcs.greaterThanFifty;
+      const res = greaterThanFifty(55);
+      expect(res).to.be.true;
+    });
+    it('should return a boolean', () => {
+      const greaterThanFifty = funcs.greaterThanFifty;
+      const res = greaterThanFifty(40);
+      expect(res).to.be.false;
     });
   });
 
   describe('`add`', () => {
     it('should be a function', () => {
       const add = funcs.add;
-      assert.typeOf(add, 'number');
+      assert.typeOf(add, 'function');
+    });
+    it('should return 5 if passed 2 and 3', () => {
+      const add = funcs.add;
+      const res = add(2, 3);
+      assert.strictEqual(res, 5);
+    });
+    it('should return a number', () => {
+      const add = funcs.add;
+      const res = add(2, 3);
+      assert.typeOf(res, 'number');
     });
   });
 
   describe('`subtract`', () => {
     it('should be a function', () => {
       const subtract = funcs.subtract;
-      assert.typeOf(subtract, 'number');
+      assert.typeOf(subtract, 'function');
+    });
+    it('should return 5 if passed 10 and 5', () => {
+      const subtract = funcs.subtract;
+      const res = subtract(10, 5);
+      assert.strictEqual(res, 5);
+    });
+    it('should return 5 if passed 10 and 5', () => {
+      const subtract = funcs.subtract;
+      const res = subtract(10, 5);
+      assert.typeOf(res, 'number');
     });
   });
 
   describe('`divide`', () => {
     it('should be a function', () => {
       const divide = funcs.divide;
-      assert.typeOf(divide, 'number');
+      assert.typeOf(divide, 'function');
+    });
+    it('should return the quotient', () => {
+      const divide = funcs.divide;
+      const res = divide(10, 5);
+      assert.strictEqual(res, 2);
+    });
+    it('should return zero if the first argument is zero', () => {
+      const divide = funcs.divide;
+      const res = divide(0, 5);
+      assert.strictEqual(res, 0);
     });
   });
 
   describe('`multiply`', () => {
     it('should be a function', () => {
       const multiply = funcs.multiply;
-      assert.typeOf(multiply, 'number');
+      assert.typeOf(multiply, 'function');
     });
+    it('should mutiply both the arguments', () => {
+      const multiply = funcs.multiply;
+      const res = multiply(5, 5);
+      assert.strictEqual(res, 25);
+    });
+    it('should return a number', () => {
+      const multiply = funcs.multiply;
+      const res = multiply(5, 5);
+      assert.typeOf(res, 'number');
+    });
+
   });
 
   describe('`getRemainder`', () => {
     it('should be a function', () => {
       const getRemainder = funcs.getRemainder;
-      assert.typeOf(getRemainder, 'number');
+      assert.typeOf(getRemainder, 'function');
+    });
+    it('should return the reminder', () => {
+      const getRemainder = funcs.getRemainder;
+      const res = getRemainder(10, 5);
+      assert.strictEqual(res, 0);
+    });
+    it('should return a number', () => {
+      const getRemainder = funcs.getRemainder;
+      const res = getRemainder(10, 5);
+      assert.typeOf(res, 'number');
     });
   });
 
   describe('`isEven`', () => {
     it('should be a function', () => {
       const isEven = funcs.isEven;
-      assert.typeOf(isEven, 'number');
+      assert.typeOf(isEven, 'function');
     });
   });
 
